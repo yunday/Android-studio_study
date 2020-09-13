29e4b99ca64ac94dfd08f649a5f34921ab5a7a33
Kyungyun Kim <kyknn79@naver.com>
2020-09-13 18:49:53 +0900

    Log 찍는 방법 반드시 알아야함!!

--- FragmentKt/app/src/main/java/com/example/fragmentkt/MainActivity.kt
+++ FragmentKt/app/src/main/java/com/example/fragmentkt/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.fragmentkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("로그테스트", "OK")
 
         setFrag(0)
 
