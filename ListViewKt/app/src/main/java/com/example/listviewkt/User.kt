5a4f3b6da925365b6fb9bb802856497e04163794
Kyungyun Kim <kyknn79@naver.com>
2020-09-14 21:09:40 +0900

    make up

--- ListViewKt/app/src/main/java/com/example/listviewkt/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.listviewkt
-
-
-/*
- 클래스 모델 객체
- (이러한 내용을 뿌리겠다고 준비해둠)
- */
-class User (val profile: Int, val name: String, val age: String, val greet: String)
--- RecyclerViewKt/app/src/main/java/com/example/recyclerviewkt/Profiles.kt
+++
@@ -1,4 +0,0 @@
-package com.example.recyclerviewkt
-
-// Profiles 객체 모델 클래스
-class Profiles(val gender:Int, val name: String, val age: Int, val job: String)
